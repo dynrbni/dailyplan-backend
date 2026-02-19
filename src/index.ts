00e5f7bbe5d90e2e648b69e0f9898f1fb73559f8
dynrbni <deanrabbani20@gmail.com>
2026-02-19 13:53:14 +0700

    feat: add CORS support with @elysiajs/cors and update root route response

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,23 @@
 import { Elysia } from "elysia";
+import { cors } from "@elysiajs/cors";
 import { jwt } from "@elysiajs/jwt";
 import userRoutes from "../src/routes/user";
 import todoRoutes from "../src/routes/todo";
 import "dotenv/config";
 
 const app = new Elysia()
+  .use(cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }))
   .use(jwt({ 
     name: "jwt",
     secret: process.env.JWT_SECRET || "default_secret",
     exp: "1d"
   }))
 
-  .get("/", () => "Hello Elysia")
+  .get("/", () => "DailyPlan API King Dean is Running!")
 
   .group("/api", (app) => {
     return app.use(userRoutes).use(todoRoutes);
